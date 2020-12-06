f79e3bbaf4637fd1bed80745009843827becff02
RyuseiNomi <kanontonto1010@gmail.com>
2020-12-06 22:44:32 +0900

    set cell position automatically

--- main.go
+++ main.go
@@ -1,27 +1,54 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"strconv"
+	"time"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
 func main() {
+	// 引数より、稼働時間を取得
+	// ex) go run main.go 8
+	flag.Parse()
+	sh := flag.Arg(0)
+	if sh == "" {
+		fmt.Println("稼働時間を入力してください。")
+		return
+	}
+	hour, _ := strconv.Atoi(sh)
+	// 実行時の月を取得
+	t := time.Now()
+	m := t.Month()
+	ms := m.String()
+	d := t.Day()
+	ds := strconv.Itoa(d)
+	log.Println(ms, ds)
 	// Excelファイルを開く
 	f, err := excelize.OpenFile("./sheet/kintai.xlsx")
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 		return
 	}
 	// シートのindex番号を取得
-	index := f.GetSheetIndex("12月")
+	index := f.GetSheetIndex(ms)
 	// 値をSet
-	f.SetCellValue("12月", "B2", 8)
+	f.SetCellValue(ms, "B"+ds, hour)
 	f.SetActiveSheet(index)
 	// 保存
 	if err := f.Save(); err != nil {
-		log.Println(err)
+		fmt.Println(err)
 		return
 	}
-	log.Println("completed to record your work record!")
+	// 合計稼働時間の取得
+	th, err := f.GetCellValue(ms, "B32")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("勤怠を記録しました。\n今月の合計稼働時間は、現在 " + th + " 時間です。\n本日もお疲れ様でした。")
 }
