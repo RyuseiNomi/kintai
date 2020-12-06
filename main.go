4206634ce8078085839305ad8a02fcc32f0e3e63
RyuseiNomi <kanontonto1010@gmail.com>
2020-12-06 22:08:21 +0900

    set a value on xlsx sheet by using excelize

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/360EntSecGroup-Skylar/excelize"
+)
 
 func main() {
-	fmt.Println("hello")
+	// Excelファイルを開く
+	f, err := excelize.OpenFile("./sheet/kintai.xlsx")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	// シートのindex番号を取得
+	index := f.GetSheetIndex("12月")
+	// 値をSet
+	f.SetCellValue("12月", "B2", 8)
+	f.SetActiveSheet(index)
+	// 保存
+	if err := f.Save(); err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("completed to record your work record!")
 }
